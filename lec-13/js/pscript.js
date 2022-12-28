a2051f6ba69b0a7561cdd70b8f884c9c7c74a162
Munisekh <munisekhar404@gmail.com>
2022-12-28 19:17:48 +0530

    code change

---
+++ lec-13/js/pscript.js
@@ -0,0 +1,7 @@
+var name = 'alina',
+ designation = 'teacher',
+ age = '22',
+ salary = '25k',
+ city = 'hyderabad';
+ document.getElementById("display-name").innerHTML = firstName;
+ console.log(name);
---
+++ lec-14/js/pscript.js
@@ -0,0 +1,21 @@
+var str = ('gfuh ieiuei')
+console.log(Str.slice(0, 6));
+
+
+var str1 = ('hduej dij')
+console.log(str1.length);
+console.log(str1.toUpperCase());
+
+
+var str2 = (' BVHDBGSH ISJI  ')
+console.log(str2.toLocaleLowerCase());
+console.log(str2.trim());
+console.log( str2.replace("ISJI","muns"));
+
+
+console.log(parseInt('8457asdf'));
+console.log(parseInt('mun5864'));
+console.log(parseInt('56.23'));
+
+console.log(parseFloat('42.24'));
+console.log(parseFloat('28.31jgnf'));
